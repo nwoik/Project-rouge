beadd4deed440adaea0ea5e1316d96b193ea57ee
Christian Bongcayao <cbongcayao64@gmail.com>
2022-01-25 17:12:57 +0000

    Setting foundations; window, frames and stuff

--- src/main/java/game/Game.java
+++ src/main/java/game/Game.java
@@ -1,5 +1,95 @@
 package game;
 
-public class Game {
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+
+public class Game extends Canvas implements Runnable{
+
+    private static final long serialVersionUID = 1L;
+
+    private boolean isRunning = false;
+    private Thread thread;
+
+    public Game(){
+        new Window(1000,563,"Wizard Game", this);
+        start();
+    }
+
+    private void start(){
+        isRunning = true;
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    private void stop() {
+        isRunning = false;
+        try{
+            thread.join();  //rejoins thread to main program
+        }catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //Yet again, game loop from notch that is perfect for any game
+    //updates window 60 times per second and updates render method
+    //a couple thousand times per second.
+    public void run() {
+        this.requestFocus();
+        long lastTime = System.nanoTime();
+        double amountOfTicks = 60.0;
+        double ns = 1000000000 / amountOfTicks;
+        double delta = 0;
+        long timer = System.currentTimeMillis();
+        int frames = 0;
+        while(isRunning){
+            long now = System.nanoTime();
+            delta += (now - lastTime) / ns;
+            lastTime = now;
+            while(delta >= 1) {
+                tick();
+                //updates++;
+                delta--;
+            }
+            render();
+            frames++;
+
+            if(System.currentTimeMillis()-timer>1000){
+                timer += 1000;
+                frames = 0;
+                //updates = 0;
+            }
+        }
+        stop();
+    }
+
+    //updates everything in the game. updated 60 times per second
+    public void tick(){
+
+    }
+
+    //renders everything in the game. updated thousands of times per second.
+    public void render(){
+        BufferStrategy bs = this.getBufferStrategy();
+        if(bs == null){
+            //preload 3 frames before they are actually shown. Improve efficiency
+            this.createBufferStrategy(3);
+            return;
+        }
+
+        Graphics g = bs.getDrawGraphics();
+        ////////////////////////////
+        //Anything between these comments will be drawn.
+
+        g.setColor(Color.green);
+        g.fillRect(0,0, 1000, 563);
+        ///////////////////////////
+        g.dispose();
+        bs.show();
+    }
+    public static void main(String[] args){
+        new Game();
+    }
 
 }
