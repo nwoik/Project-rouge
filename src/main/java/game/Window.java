d5e865841c1f9d66976527681e8e04569e23566a
Christian Bongcayao <cbongcayao64@gmail.com>
2022-01-26 09:33:52 +0000

    cleaning window

---
+++ src/main/java/game/Window.java
@@ -0,0 +1,19 @@
+package game;
+
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class Window extends JFrame{
+    public Window(int width, int height, String title, Game game){
+        setTitle(title);
+        setPreferredSize(new Dimension(width, height));
+        setMinimumSize(new Dimension(width, height));
+        setMaximumSize(new Dimension(width, height));
+        add(game);
+        setResizable(false);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+}
