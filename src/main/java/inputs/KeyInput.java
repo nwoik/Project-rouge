3609b51c0124cf2b70d685ce1a4e9730ddc41bdc
Christian Bongcayao <cbongcayao64@gmail.com>
2022-02-15 09:48:47 +0000

    keybinds work

--- src/main/java/inputs/KeyInput.java
+++ src/main/java/inputs/KeyInput.java
@@ -7,14 +7,44 @@ import window.Game;
 
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class KeyInput extends KeyAdapter{
     Game game;
+    ArrayList<Integer> keysList = new ArrayList<Integer>();
     Handler handler; //Note: not creating new handler because we need to be using same handler as before
     //                 Don't want a new list, just to access movement methods.
     //                 Reason for putting movement there is to prevent input lag.
 
-    public KeyInput(Handler handler) {this.handler = handler;}
+    public KeyInput(Handler handler) {
+        this.handler = handler;
+        this.keysList=keysList;
+
+        try {
+            readText(this.keysList);
+        }
+        catch(IOException a) {
+            a.printStackTrace();
+        }
+    }
+
+    public void readText(ArrayList<Integer> keysList) throws IOException {
+        short inc = 0;
+        File file = new File("src/main/java/window/menu/Settings.txt");
+        String line;
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        while ((line = reader.readLine()) != null) {
+            if (inc >= 4) {
+                int key=Integer.parseInt(line);
+                keysList.add(key);
+            }
+            inc ++;
+        }
+    }
 
     public void keyPressed(KeyEvent e) {
         int key = e.getKeyCode();
@@ -24,10 +54,10 @@ public class KeyInput extends KeyAdapter{
 
             //loop through all our objects, and if the current object is the player then do something.
             if (tempObject.getId() == ID.Player) {
-                if (key == KeyEvent.VK_W) handler.setUp(true);
-                if (key == KeyEvent.VK_S) handler.setDown(true);
-                if (key == KeyEvent.VK_A) handler.setLeft(true);
-                if (key == KeyEvent.VK_D) handler.setRight(true);
+                if (key == this.keysList.get(0)) handler.setUp(true);
+                if (key == this.keysList.get(1)) handler.setDown(true);
+                if (key == this.keysList.get(2)) handler.setLeft(true);
+                if (key == this.keysList.get(3)) handler.setRight(true);
             }
         }
     }
@@ -40,10 +70,10 @@ public class KeyInput extends KeyAdapter{
 
             //loop through all our objects, and if the current object is the player then do something.
             if(tempObject.getId() == ID.Player){
-                if(key == KeyEvent.VK_W) handler.setUp(false);
-                if(key == KeyEvent.VK_S) handler.setDown(false);
-                if(key == KeyEvent.VK_A) handler.setLeft(false);
-                if(key == KeyEvent.VK_D) handler.setRight(false);
+                if(key == this.keysList.get(0)) handler.setUp(false);
+                if(key == this.keysList.get(1)) handler.setDown(false);
+                if(key == this.keysList.get(2)) handler.setLeft(false);
+                if(key == this.keysList.get(3)) handler.setRight(false);
             }
         }
     }
