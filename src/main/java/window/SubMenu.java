d752ea7bc6f00ce125ad53e7f4849393889f97cc
Christian Bongcayao <cbongcayao64@gmail.com>
2022-03-01 18:42:11 +0000

    first commit in subMenu

--- src/main/java/window/SubMenu.java
+++ src/main/java/window/SubMenu.java
@@ -1,18 +1,51 @@
 package window;
 
+import audio.AudioHandler;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class SubMenu extends JPanel {
-    public SubMenu() {
+    private SubWindow subWindow;
+    private GameCanvas gameCanvas;
+    public SubMenu(SubWindow subWindow, GameCanvas gameCanvas) {
+        this.subWindow = subWindow;
+        this.gameCanvas = gameCanvas;
+
         setBackground(new Color(255, 200, 200));
-        Canvas canvas = new Canvas();
-        canvas.setSize(400, 400);
-        add(canvas);
+        setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+
+        JButton returnButton = new JButton("Return");
+        JButton exitButton = new JButton("Exit");
+
+        returnButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                closeMenu();
+                gameCanvas.comeBack();
+            }
+        });
+
+        exitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                subWindow.dispose();
+            }
+        });
+
+        add(Box.createRigidArea(new Dimension(0, 400)));
+        add(returnButton);
+        add(Box.createRigidArea(new Dimension(0, 100)));
+        add(exitButton);
+
+        returnButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        exitButton.setAlignmentX(Component.CENTER_ALIGNMENT);
     }
 
-    public void paint(Graphics g) {
-        g.fillOval(100, 100, 200, 200);
+    public void closeMenu() {
+        this.subWindow.dispose();
+        this.gameCanvas.stopped = !this.gameCanvas.stopped;
     }
-
 }
