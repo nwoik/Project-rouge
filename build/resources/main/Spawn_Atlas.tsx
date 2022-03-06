2767bcc0ed73280fecbc5f315ca3b954d5861698
nwoik <nwokoyed@gmail.com>
2022-03-06 19:53:19 +0000

    Added spawn layer

---
+++ build/resources/main/Spawn_Atlas.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="Spawn_Atlas" tilewidth="16" tileheight="16" tilecount="4" columns="1">
+ <image source="Spawn_Atlas.png" width="16" height="64"/>
+</tileset>
