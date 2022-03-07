48c87fcea8b69266a30814aace213ec8a58dd549
Christian Bongcayao <cbongcayao64@gmail.com>
2022-03-07 11:41:40 +0000

    game over screen created

---
+++ src/main/java/window/GameOverScreen.java
@@ -0,0 +1,64 @@
+package window;
+
+import audio.AudioHandler;
+import core.BufferedImageLoader;
+import window.menu.LayoutPanel;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+
+public class GameOverScreen extends JPanel {
+    public GameOverWindow gameOverWindow;
+    public GameCanvas gameCanvas;
+    private GameWindow gameWindow;
+    private LayoutPanel layoutPanel;
+
+    public GameOverScreen(GameOverWindow gameOverWindow, GameCanvas gameCanvas, GameWindow gameWindow, LayoutPanel layoutPanel) {
+        this.gameOverWindow = gameOverWindow;
+        this.gameCanvas = gameCanvas;
+        this.gameWindow = gameWindow;
+        this.layoutPanel = layoutPanel;
+
+        setBackground(new Color(255, 200, 200));
+        setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+
+        BufferedImageLoader image = new BufferedImageLoader();
+        BufferedImage wordAtlas = image.loadImage("/Word Sheet.png");
+
+        AudioHandler clickAudio = new AudioHandler();
+
+        Widget exitButton = new Widget(wordAtlas.getSubimage(0,36,31,12), wordAtlas.getSubimage(74,36,31,12));
+        exitButton.scaleWidget(200, 60);
+
+        exitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                clickAudio.playSFX("sfx/menu/wood_click.wav");
+                gameOverWindow.dispose();
+                gameWindow.remove(gameCanvas);
+                layoutPanel.setVisible(true);
+            }
+        });
+
+        add(Box.createRigidArea(new Dimension(0, 100)));
+        add(exitButton);
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+        int HEIGHT = dimension.height;
+        int WIDTH = dimension.width;
+
+        BufferedImageLoader bufferLoader = new BufferedImageLoader();
+        Graphics2D graphics2D = (Graphics2D)g;
+        for (int xx=0; xx<WIDTH; xx+=128){
+            for(int yy = 0; yy<HEIGHT; yy+=128){
+                graphics2D.drawImage(bufferLoader.loadImage("/MenuBackground/Wood Texture Bottom side.png"), xx,yy, 128, 128, null);
+            }
+        }
+    }
+}
