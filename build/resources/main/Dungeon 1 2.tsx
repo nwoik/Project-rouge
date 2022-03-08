20f3fd29a0956ecaaa11737fdb17019dc68f28fc
guzzmle <73357188+guzzmle@users.noreply.github.com>
2022-03-08 12:21:22 +0000

    added player death sfx

---
+++ build/resources/main/Dungeon 1 2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="Dungeon 1" tilewidth="16" tileheight="16" tilecount="30" columns="5">
+ <image source="Dungeon 1 atlas.png" width="80" height="96"/>
+</tileset>
